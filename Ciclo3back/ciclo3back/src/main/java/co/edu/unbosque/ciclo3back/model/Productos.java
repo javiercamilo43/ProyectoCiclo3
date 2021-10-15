45d24c73dc337f950ce5ffc766921ee0a8c287cd
EmersonUrielVargas <71224875+EmersonUrielVargas@users.noreply.github.com>
2021-10-14 23:17:11 -0500

    Primer Incremento en git

---
+++ Ciclo3back/ciclo3back/src/main/java/co/edu/unbosque/ciclo3back/model/Productos.java
@@ -0,0 +1,51 @@
+package co.edu.unbosque.ciclo3back.model;
+
+public class Productos {
+	
+	private long codigo_producto;
+	private double ivacompra;
+	private long nitproveedor;
+	private String nombre_producto;
+	private double precio_compra;
+	private double precio_venta;
+	
+	public long getCodigo_producto() {
+		return codigo_producto;
+	}
+	public void setCodigo_producto(long codigo_producto) {
+		this.codigo_producto = codigo_producto;
+	}
+	public Double getIvacompra() {
+		return ivacompra;
+	}
+	public void setIvacompra(Double ivacompra) {
+		this.ivacompra = ivacompra;
+	}
+	public long getNitproveedor() {
+		return nitproveedor;
+	}
+	public void setNitproveedor(long nitproveedor) {
+		this.nitproveedor = nitproveedor;
+	}
+	public String getNombre_producto() {
+		return nombre_producto;
+	}
+	public void setNombre_producto(String nombre_producto) {
+		this.nombre_producto = nombre_producto;
+	}
+	public Double getPrecio_compra() {
+		return precio_compra;
+	}
+	public void setPrecio_compra(Double precio_compra) {
+		this.precio_compra = precio_compra;
+	}
+	public Double getPrecio_venta() {
+		return precio_venta;
+	}
+	public void setPrecio_venta(Double precio_venta) {
+		this.precio_venta = precio_venta;
+	}
+	
+	
+
+}
